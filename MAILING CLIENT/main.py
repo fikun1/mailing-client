cdbd17fe3fb3122030d9d44240dfe90b73e5e5c0
DavidO <fikun@DavidOs-MacBook-Air.local>
2026-01-13 10:31:49 -0600

    Addition of sender & recipient

--- MAILING CLIENT/main.py
+++ MAILING CLIENT/main.py
@@ -1,5 +1,10 @@
 import smtplib
 from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 
@@ -9,4 +14,11 @@ with open('password.txt', 'r') as f:
     password = f.read()
 
 server.login('testinprojects7@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['From'] = 'testinprojects7@gmail.com'
+msg['To'] = 'davidoladejo200@gmail.com'
+msg['Subject'] = 'Test Email'
+
+
 
